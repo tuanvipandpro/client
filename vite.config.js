eb3064c741fdda6d188e95668f73733f5431e177
Lê Minh Tuấn <tuanvipandpro@gmail.com>
2022-08-30 21:24:22 +0700

    build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig({,
+  base: '/client/',
   plugins: [vue()]
 })
