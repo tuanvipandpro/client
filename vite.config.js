e642c386977a44731e8c327b061f57de032584ba
Lê Minh Tuấn <tuanvipandpro@gmail.com>
2022-09-01 09:13:07 +0700

    build#13

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/client',
+  base: process.env.NODE_ENV === 'production' ? '/client/' : '/',
   plugins: [vue()],
   build: {
     outDir: 'docs'
